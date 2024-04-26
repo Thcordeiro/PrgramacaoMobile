cb56be8c33575e9f854f147cf593a148838c23bd
Thcordeiro <thiagocordeiro5678@gmail.com>
2024-04-26 13:52:22 -0300

    Aulas Anteriores
    Aulas Anteriores

--- exercicios/src/components/Frag.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Text } from "react-native";
-
-export default (props: any) => {
-    <>
-        <Text>{props.titulo}</Text>
-        <Text>{props.subtitulo}</Text>
-    </>
-}
--- exercicios/src/components/teste.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'; 
-import { Text } from 'react-native';
-import Padrao from './Styles/Padrao';
-
-export default(props: any) => {
-  return (
-     <Text> </Text>
-   )
- }
